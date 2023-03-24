87714716a90fbabb7d9f53dce714a99ffce2c6e0
MatthD <matthias.dieudonne@gmail.com>
2023-03-24 15:48:48 +0100

    feat(minify-url) start url minification service

--- back-end/src/url-shortener/url-shortener.service.ts
+++ back-end/src/url-shortener/url-shortener.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import {AES} from 'crypto-js';
+import { parse } from 'url';
 
 @Injectable()
-export class UrlShortenerService {}
+export class UrlShortenerService {
+
+  public minifyUrl(url: string): string {
+    const key = 'key2';
+    const appUrl = 'http://localhost:3000';
+    const minified = AES.encrypt(url, key);
+    const shortUrl = `${appUrl}/${key}`;
+    return shortUrl;
+  }
+
+  public resolveUrl(shortUrl: string): string {
+    const {pathname} = new URL(shortUrl)
+    const decryptedShortUrl = AES.decrypt(pathname, 'key2');
+    return decryptedShortUrl.toString();
+  }
+}
