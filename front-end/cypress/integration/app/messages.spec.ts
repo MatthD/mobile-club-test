dea300362239d7fc0886bf555befb779dcdd0d5f
MatthD <matthias.dieudonne@gmail.com>
2023-03-25 16:41:13 +0100

    feat(url-pointer) on front we can click on url now

--- front-end/cypress/integration/app/messages.spec.ts
+++ front-end/cypress/integration/app/messages.spec.ts
@@ -1,5 +1,5 @@
 describe('should add a message', () => {
-  it('should ', () => {
+  it('should be able to add a message and show it', () => {
     const randomNumber = Math.round(Math.random() * 10);
     const message = `MobileClub!${randomNumber}`;
 
@@ -7,5 +7,17 @@ describe('should add a message', () => {
     cy.dataCy('messageInput').type(message);
     cy.dataCy('submit').click();
     cy.dataCy('messageContainer').contains(message);
+  });
+  it('should be able to add a message with url and shortner it', () => {
+    const randomNumber = Math.round(Math.random() * 10);
+    const message = `MobileClub!${randomNumber} is at http://mobile.club`;
+    cy.visit('http://localhost:3000');
+    cy.dataCy('messageInput').type(message);
+    cy.dataCy('submit').click();
+    cy.dataCy('messageContainer').should(
+      'not.include.text',
+      'http://mobile.club',
+    );
+    cy.dataCy('messageContainer').contains('http://localhost:4000');
   });
 });
